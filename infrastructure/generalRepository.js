5bb399f3fade1fb948684437042ae2484e953827
Alex <lxalonso@gmail.com>
2021-06-04 11:20:41 +0200

    v1 Añadir entrada en tablas ok, estructura ok

---
+++ infrastructure/generalRepository.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs')
+const { getConnection } = require('./bd/db')
+
+const save = async (user, table) => {
+    user.password = bcrypt.hashSync(user.password, 10)
+    const cont = Object.values(user).length
+    let cadena = '?'
+    for (let i=1; i<cont; i++){
+        cadena = cadena +', ?'
+    }
+    const keys = Object.keys(user).toString()
+    const connection = getConnection()
+    let sentencia = `INSERT INTO ${table} (${keys}) VALUES (${cadena})`
+    const values  = Object.values(user).map(value=> (typeof(value)==='string'? value = "'" + value + "'": value))
+    console.log(keys)
+    console.log(Object.values(user))
+    await connection.query(sentencia,Object.values(user))
+}
+
+module.exports = {
+    save
+}
--- infrastructure/userRepository.js
+++ infrastructure/userRepository.js
@@ -1,18 +1,9 @@
-const bcrypt = require('bcryptjs')
+
+// FUNCTION SAVE IN generalRepository.js
+
 const { getConnection } = require('./bd/db')
 
-const save = async (user, table) => {
-    
-        user.password = bcrypt.hashSync(user.password, 10)
-        const prepareStrintToDb = Object.values(user).map(value => {
-            return (!typeof (value) === 'string' ? value : "'" + value + "'") //Prepara las string para bd, añadiendo ' 
-        });
-        const keys = Object.keys(user).toString()
-        const values = prepareStrintToDb.toString()
-        const connection = getConnection()
-        await connection.query(`INSERT INTO ${table} (${keys}) VALUES (${values});`)
-    
-}
+
 
 const find = async ({ }) => {
 
@@ -22,6 +13,3 @@ const get = async (id) => {
 
 }
 
-module.exports = {
-    save
-}
