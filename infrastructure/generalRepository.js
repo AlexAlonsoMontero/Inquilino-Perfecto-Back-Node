28f4064838b41133936181575e37b962bdca2c1b
VanessaCV <erioyaki@gmail.com>
2021-06-05 12:27:53 +0200

    Añadido comentario y cambiado nombre de parámetro para 'save'

--- infrastructure/generalRepository.js
+++ infrastructure/generalRepository.js
@@ -1,18 +1,23 @@
 const bcrypt = require('bcryptjs')
 const { getConnection } = require('./bd/db')
 
-const save = async (user, table) => {
-    user.password = bcrypt.hashSync(user.password, 10)
-    const cont = Object.values(user).length
+/**
+ * Guarda en la base de datos los datos correspondientes a la entidad dada
+ * @param {json} entity correspondiente al dato a insertar
+ * @param {string} table correspondiente a dónde será insertado el dato
+ */
+const save = async (entity, table) => {
+    entity.password = bcrypt.hashSync(entity.password, 10)
+    const cont = Object.values(entity).length
     let cadena = '?'
     for (let i=1; i<cont; i++){
         cadena = cadena +', ?'
     }
-    const keys = Object.keys(user).toString()
+    const keys = Object.keys(entity).toString()
     const connection = getConnection()
     let sentencia = `INSERT INTO ${table} (${keys}) VALUES (${cadena})`
-    const values  = Object.values(user).map(value=> (typeof(value)==='string'? value = "'" + value + "'": value))
-    await connection.query(sentencia,Object.values(user))
+    const values  = Object.values(entity).map(value=> (typeof(value)==='string'? value = "'" + value + "'": value))
+    await connection.query(sentencia,Object.values(entity))
 }
 
 module.exports = {
