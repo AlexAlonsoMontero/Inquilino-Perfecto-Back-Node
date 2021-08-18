c580c0383c92285fa700fc876bc396b5d7b0b839
alex <lxalonso@gmail.com>
2021-08-18 13:21:33 +0200

    Modificados errores para formato de datos en registro

--- customErrors/errorInvalidField.js
+++ customErrors/errorInvalidField.js
@@ -13,7 +13,12 @@ class errorInvalidField extends Error {
         this.name = "errorInvalidField";
         this.type = "joi";
         this.message = `Not data valid in '${where}' where '${what1}' is '${what2}', because '${why}'`;
-        this.messageEsp = `Formato  ${what1} incorrecto `;
+        if(this.type==="joi"){
+            this.messageEsp =`Formato  ${what1} incorrecto `
+        }else{
+            this.messageEsp = `Formato  ${what1} incorrecto `;
+        }
+        
     }
 }
 
